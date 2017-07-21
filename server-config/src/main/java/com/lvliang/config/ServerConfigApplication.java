fa95359ccf470120e23c1febe9890c7467532604
Liang Lv <sunjavaer@gmail.com>
2017-07-21 13:53:06 +0800

    test

--- server-config/src/main/java/com/lvliang/config/ServerConfigApplication.java
+++ server-config/src/main/java/com/lvliang/config/ServerConfigApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * test main
+ * @author thinkpad
+ *
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class ServerConfigApplication {
